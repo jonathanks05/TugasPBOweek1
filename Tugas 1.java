ae9a2eea5352c7db5319a150378ba333c441030a
jonathan karel <111202013053@mhs.dinus.ac.id>
2025-03-08 14:02:45 +0700

    Add files via upload

---
+++ Tugas 1.java
@@ -0,0 +1,12 @@
+public class Tugas1 {
+    public static void main(String[] args) {
+        System.out.println("NIM      : A11.2020.13053");
+        System.out.println("Nama     : Jonathan Karel Setiawan");
+        System.out.println("Alamat   : Jl. ringroad No. 123");
+        System.out.println("Kota     : jogja");
+        System.out.println("Kode Pos : 12345");
+        System.out.println("Telp.    : 021-1234567");
+        System.out.println("HP       : 08123456789");
+        System.out.println("Email    : jojo@example.com");
+    }
+}
